8e0c07eeb1570ea922edda5d5fbcbad2bbc37b04
Aryansharma-aryan <157520977+Aryansharma-aryan@users.noreply.github.com>
2025-06-16 23:20:55 +0530

    Update DisplayData.jsx

--- client/src/pages/DisplayData.jsx
+++ client/src/pages/DisplayData.jsx
@@ -12,7 +12,7 @@ const DisplayData = ({ setCart = () => {} }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:3102/api/food`, {
+        const response = await fetch(`https://mern-stack-food-ordering-web-app-2.onrender.com/api/food`, {
           mode: "cors",
           credentials: "include",
         });
