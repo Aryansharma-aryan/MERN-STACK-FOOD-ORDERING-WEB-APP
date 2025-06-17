bb0e2591145c647c31da0cd313b62efe977734b4
Aryansharma-aryan <157520977+Aryansharma-aryan@users.noreply.github.com>
2025-06-17 16:15:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const server = http.createServer(app);
 // 1️⃣ Priority CORS + preflight
 const allowedOrigins = [
   /^https?:\/\/localhost(:\d+)?$/,
-  "https://mern-stack-food-ordering-web-ny25yptdl.vercel.app",
+  "https://mern-stack-food-ordering-web-app.vercel.app
+",
   
 ];
 
